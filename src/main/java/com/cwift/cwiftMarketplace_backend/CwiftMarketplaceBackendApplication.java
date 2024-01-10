68667868f4bfe27014bebb39f85b7b37f5f4d471
Chris-Mwesigwa <cmwesigwa@aetotechs.com>
2024-01-10 14:07:34 +0300

    innitial commit

---
+++ src/main/java/com/cwift/cwiftMarketplace_backend/CwiftMarketplaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cwift.cwiftMarketplace_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwiftMarketplaceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwiftMarketplaceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cwift/cwiftMarketplace_backend/CwiftMarketplaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwift.cwiftMarketplace_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwiftMarketplaceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
